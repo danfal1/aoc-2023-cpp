7a8f5f2831b7e19585a173dfcc29c54cc6f057ca
Daniel Faludi <danielfaludi2@gmail.com>
2023-12-09 13:26:44 +0100

    cleanup

--- day3/day3.cpp
+++ day3/day3.cpp
@@ -1,5 +1,7 @@
+#include "day3.hpp"
+
 #include <cctype>
-#include <cstddef>
+#include <exception>
 #include <fstream>
 #include <functional>
 #include <numeric>
@@ -10,40 +12,6 @@
 #include <vector>
 #include <unordered_map>
 
-typedef std::vector<std::vector<char>> Schema;
-
-struct Symbol {
-    unsigned x;
-    unsigned y;
-    char c;
-
-    bool operator==(const Symbol& other) const {
-        return x == other.x && y == other.y && c == other.c;
-    }
-};
-
-template <>
-struct std::hash<Symbol> {
-    std::size_t operator()(const Symbol& s) const {
-        using std::size_t;
-        using std::hash;
-        using std::string;
-
-        // Compute individual hash values for first,
-        // second and third and combine them using XOR
-        // and bit shifting:
-
-        return ((hash<unsigned>()(s.x)
-                 ^ (hash<unsigned>()(s.y) << 1)) >> 1)
-                 ^ (hash<char>()(s.c) << 1);
-        }
-};
-
-typedef struct {
-    std::string str;
-    Symbol symbol;
-} Number;
-
 std::ifstream get_file_stream(std::string file_name) {
     std::ifstream ifs(file_name);
     if (!ifs) {
@@ -52,7 +20,7 @@ std::ifstream get_file_stream(std::string file_name) {
     return ifs;
 }
 
-bool is_symbol(char c) {
+bool is_special_char(char c) {
     return c != '.' && std::ispunct(c);
 }
 
@@ -64,56 +32,56 @@ std::optional<Symbol> get_symbol(Schema& s, unsigned x, unsigned y) {
     char c;
     if (x > 0) {
         c = s[y][x - 1];
-        if (is_symbol(c)) {
+        if (is_special_char(c)) {
             return make_symbol(c, x - 1, y);
         }
     }
 
     if (x < s[y].size() - 1) {
         c = s[y][x + 1];
-        if (is_symbol(c)) {
+        if (is_special_char(c)) {
             return make_symbol(c, x + 1, y);
         }
     }
 
     if (y > 0) {
         c = s[y - 1][x];
-        if (is_symbol(c)) {
+        if (is_special_char(c)) {
             return make_symbol(c, x, y - 1);
         }
     } 
 
     if (y < s.size() - 1) {
         c = s[y + 1][x];
-        if (is_symbol(c)) {
+        if (is_special_char(c)) {
             return make_symbol(c, x, y + 1);
         }
     }
 
     if (x > 0 && y > 0) {
         c = s[y - 1][x - 1];
-        if (is_symbol(c)) {
+        if (is_special_char(c)) {
             return make_symbol(c, x - 1, y - 1);
         }
     }
 
     if (x < s[y].size() - 1 && y > 0) {
         c = s[y - 1][x + 1];
-        if (is_symbol(c)) {
+        if (is_special_char(c)) {
             return make_symbol(c, x + 1, y - 1);
         }
     }
 
     if (x > 0 && y < s.size() - 1) {
         c = s[y + 1][x - 1];
-        if (is_symbol(c)) {
+        if (is_special_char(c)) {
             return make_symbol(c, x - 1, y + 1);
         }
     }
 
     if (x < s[y].size() - 1 && y < s.size() - 1) {
         c = s[y + 1][x + 1];
-        if (is_symbol(c)) {
+        if (is_special_char(c)) {
             return make_symbol(c, x + 1, y + 1);
         }
     }
@@ -136,12 +104,8 @@ std::unordered_map<Symbol, std::vector<int>> build_map(Schema& s) {
                 }
             } else {
                 if (n.symbol.c) {
-                    if (auto search = m.find(n.symbol); search != m.end()) {
-                        m[n.symbol].push_back(std::stoi(n.str));
-                    } else {
-                        m[n.symbol] = {std::stoi(n.str)};
-                    }
-                } 
+                    m[n.symbol].push_back(std::stoi(n.str));
+                }
                 n = {};
             }
         }
@@ -153,11 +117,18 @@ std::unordered_map<Symbol, std::vector<int>> build_map(Schema& s) {
 int main(int argc, char **argv) {
     if (argc < 2) {
         std::cerr << "no input file" << std::endl;
-        return -1;
+        return 1;
     }
 
     std::string file_name = argv[1];
-    std::ifstream file_stream = get_file_stream(file_name);
+    std::ifstream file_stream;
+    
+    try {
+        file_stream = get_file_stream(file_name);
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return 2;
+    }
 
     Schema s;
     std::string line;
@@ -173,18 +144,10 @@ int main(int argc, char **argv) {
     int result_part_2 = 0;
 
     auto schema_map = build_map(s);
-    // for (auto it = schema_map.begin(); it != schema_map.end(); it++) {
-    //     std::cout << "Symbol: " << it->first.c << ": ";
-    //     for (auto v : it->second) {
-    //         std::cout << v << " ";
-    //     }
-    //     std::cout << '\n';
-    // }
-
-    for (auto it = schema_map.begin(); it != schema_map.end(); it++) {
-        result_part_1 += std::accumulate(it->second.begin(), it->second.end(), 0);
-        if (it->first.c == '*') {
-            result_part_2 += std::accumulate(it->second.begin(), it->second.end(), 1, std::multiplies<int>());
+    for (const auto& [sym, vec] : schema_map) {
+        result_part_1 += std::accumulate(vec.begin(), vec.end(), 0);
+        if (sym.c == '*' && vec.size() == 2) {
+            result_part_2 += std::accumulate(vec.begin(), vec.end(), 1, std::multiplies<int>());
         }
     }
 
